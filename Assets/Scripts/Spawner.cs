43bbe668f74101c09cdb1dbaa30fb2b5819c8806
Tasos Akoumianakis <tasosakoum123d@gmail.com>
2024-09-23 15:50:35 +0300

    Final commit

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class Spawner : MonoBehaviour {
     public float secondsToSpawn;
     public GameObject prefabToSpawn;
 
     public static int itemsSpawned = 0;
+
+    private string outputFile = @"C:\Users\tasos\Downloads\unity_benchmark.txt";
     private void Start() {
         StartCoroutine(SpawnPrefabEverySecond());
+    }
+
+    private void Update() {
+        var currentFps = (int)(1f / Time.unscaledDeltaTime);
+
+        if (itemsSpawned % 50 == 0) {
+            var barrelsToFps = $"({itemsSpawned}, {currentFps})\n";
+            
+            File.AppendAllText(outputFile, barrelsToFps);
+        }
     }
 
     private IEnumerator SpawnPrefabEverySecond() {
